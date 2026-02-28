c325384ae455ba0692f16501fd750a5a2bf9abb3
Jaju8756 <jaju8756@colorado.edu>
2026-02-27 18:31:27 -0700

    added init scripts; kernel oops trigger; fulltest validated

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,4 +1,4 @@
-LDD_VERSION = '225123efd85147606a9e1f4ea28cd2bdc4a169a7'
+LDD_VERSION = f96b08c14fd1ec0547a6af0db78460d0e89c6328
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-Jaju8756.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
@@ -7,7 +7,6 @@ LDD_LICENSE = GPL-2.0
 LDD_LICENSE_FILES = LICENSE
 
 LDD_MODULE_SUBDIRS = misc-modules scull
-LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
